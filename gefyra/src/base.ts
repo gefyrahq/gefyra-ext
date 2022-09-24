cce270263fa273de361bed2b815eea94722fa2a6
Robert Stein <robert@blueshoe.de>
2022-09-24 09:05:44 +0200

    chore: add missing types / refactor client

---
+++ gefyra/src/base.ts
@@ -0,0 +1,23 @@
+import * as proto from './protocol';
+
+export abstract class GefyraBaseClient {
+  protected abstract exec(request: proto.GefyraRequest): Promise<string>
+
+  async status(): Promise<proto.GefyraStatusResponse> {
+    const stdout = this.exec(new proto.GefyraStatusRequest());
+    const resp = await stdout;
+    return new proto.GefyraStatusResponse(resp);
+  }
+
+  async up(): Promise<proto.GefyraUpResponse> {
+    const stdout = this.exec(new proto.GefyraUpRequest());
+    const resp = await stdout;
+    return new proto.GefyraUpResponse(resp);
+  }
+
+  async down(): Promise<proto.GefyraDownResponse> {
+    const stdout = this.exec(new proto.GefyraDownRequest());
+    const resp = await stdout;
+    return new proto.GefyraDownResponse(resp);
+  }
+}
--- gefyra/src/gefyra.ts
+++ gefyra/src/gefyra.ts
@@ -1,37 +1,19 @@
-import { join } from 'path';
-import { binaryPath } from './install';
 import * as proto from './protocol';
-import util = require('util');
+import { promisify } from 'util';
 import { exec } from 'child_process';
-const pexec = util.promisify(exec);
+import { GefyraBaseClient } from './base';
+const pexec = promisify(exec);
 
-export class Gefyra {
+export class Gefyra extends GefyraBaseClient {
   private binary: string;
 
   constructor(binary: string) {
+    super()
     this.binary = binary;
   }
 
-  protected async exec(request: proto.GefyraRequest) {
+  protected async exec(request: proto.GefyraRequest): Promise<string> {
     const { stdout } = await pexec(`${this.binary} '${request.serialize()}'`);
     return stdout;
-  }
-
-  async status(): Promise<proto.GefyraStatusResponse> {
-    const stdout = this.exec(new proto.GefyraStatusRequest());
-    const resp = await stdout;
-    return new proto.GefyraStatusResponse(resp);
-  }
-
-  async up(): Promise<proto.GefyraUpResponse> {
-    const stdout = this.exec(new proto.GefyraUpRequest());
-    const resp = await stdout;
-    return new proto.GefyraUpResponse(resp);
-  }
-
-  async down(): Promise<proto.GefyraDownResponse> {
-    const stdout = this.exec(new proto.GefyraDownRequest());
-    const resp = await stdout;
-    return new proto.GefyraDownResponse(resp);
   }
 }
