974e8c60739288f7b7f227e2fbdb3c8fb43724f1
Tobias Frölich <tobias@blueshoe.de>
2022-10-27 08:35:47 +0200

    feat: add run, help, bridge

--- gefyra/src/protocol.ts
+++ gefyra/src/protocol.ts
@@ -17,7 +17,9 @@ export class GefyraStatusRequest extends GefyraRequest {
 }
 
 export class GefyraUpRequest extends GefyraRequest {
-  
+  endpoint?: string;
+  minikube?: boolean;
+
   constructor() {
     super();
     this.action = 'gefyra.up';
@@ -28,6 +30,47 @@ export class GefyraDownRequest extends GefyraRequest {
   constructor() {
     super();
     this.action = 'gefyra.down';
+  }
+}
+
+export class GefyraRunRequest extends GefyraRequest {
+  image!: string;
+  name?: string;
+  command?: string;
+  // volumes and ports are tbd
+  volumes?: string[];
+  ports?: string[];
+  detach?: boolean;
+  autoremove?: boolean;
+  namespace?: string;
+  env?: string[];
+  envfrom?: string;
+  
+  constructor() {
+    super();
+    this.action = 'gefyra.run';
+  }
+}
+
+export class GefyraBridgeRequest extends GefyraRequest {
+  name!: string;
+  ports!: string[];
+  target!: string;
+  namespace?: string;
+  syncDownDirs?: string[];
+  handleprobes?: boolean;
+  timeout?: Number;
+
+  constructor() {
+    super();
+    this.action = 'gefyra.bridge'
+  }
+}
+
+export class GefyraHelpRequest extends GefyraRequest {
+  constructor() {
+    super();
+    this.action = 'help';
   }
 }
 
@@ -76,4 +119,55 @@ export class GefyraDownResponse extends GefyraResponse {
     const obj = this.deserialize(res);
     this.status = obj.status;
   }
+}
+
+export class GefyraRunResponse extends GefyraResponse {
+  status!: string;
+  host!: string;
+  user!: string;
+  apiVersion!: string;
+  version!: string;
+  response!: GefyraRunInnerResponse; 
+  
+  constructor(res: string) {
+    super();
+    const obj = this.deserialize(res)
+    this.status = obj.status;
+    this.host = obj.host;
+    this.user = obj.user;
+    this.apiVersion = obj.apiVersion;
+    this.version = obj.version;
+    this.response = obj.response;
+  }
+}
+
+export class GefyraBridgeResponse extends GefyraResponse {
+  status!: string;
+  host!: string;
+  user!: string;
+  apiVersion!: string;
+  version!: string;
+  response!: GefyraBridgeInnerResponse; 
+
+  constructor(res: string) {
+    super();
+    const obj = this.deserialize(res)
+    this.status = obj.status;
+    this.host = obj.host;
+    this.user = obj.user;
+    this.apiVersion = obj.apiVersion;
+    this.version = obj.version;
+    this.response = obj.response;
+  }
+}
+
+
+// TODO Move somewhere else
+type GefyraRunInnerResponse = {
+	status: boolean; 
+}
+
+type GefyraBridgeInnerResponse = {
+  containers: string[],
+  bridges: string[]
 }
