b88fa3e2fe39c1e72b8f0f77c03e2fba54f1ec49
Tobias Frölich <40638719+tobifroe@users.noreply.github.com>
2022-11-03 14:39:08 +0100

    feat: add up, run, help, bridge (#2)
    * feat: add run, help, bridge
    * chore: move types to single file
    * chore: move more members to base gefyraResponse
    * fix: use correct number type

--- gefyra/src/protocol.ts
+++ gefyra/src/protocol.ts
@@ -1,3 +1,4 @@
+import {GefyraRunInnerResponse, GefyraBridgeInnerResponse} from './types'
 /* tslint:disable:max-classes-per-file */
 // ===== API Requests ====
 
@@ -17,7 +18,9 @@ export class GefyraStatusRequest extends GefyraRequest {
 }
 
 export class GefyraUpRequest extends GefyraRequest {
-  
+  endpoint?: string;
+  minikube?: boolean;
+
   constructor() {
     super();
     this.action = 'gefyra.up';
@@ -31,12 +34,55 @@ export class GefyraDownRequest extends GefyraRequest {
   }
 }
 
+export class GefyraRunRequest extends GefyraRequest {
+  image!: string;
+  name?: string;
+  command?: string;
+  // volumes and ports are tbd
+  volumes?: string[];
+  ports?: string[];
+  detach?: boolean;
+  autoremove?: boolean;
+  namespace?: string;
+  env?: string[];
+  envfrom?: string;
+  
+  constructor() {
+    super();
+    this.action = 'gefyra.run';
+  }
+}
+
+export class GefyraBridgeRequest extends GefyraRequest {
+  name!: string;
+  ports!: string[];
+  target!: string;
+  namespace?: string;
+  syncDownDirs?: string[];
+  handleprobes?: boolean;
+  timeout?: number;
+
+  constructor() {
+    super();
+    this.action = 'gefyra.bridge'
+  }
+}
+
+export class GefyraHelpRequest extends GefyraRequest {
+  constructor() {
+    super();
+    this.action = 'help';
+  }
+}
+
 // ===== APi Respones ====
 class GefyraResponse {
   status!: string;
-  success!: boolean;
-  host!: string;
-  user!: string;
+  success?: boolean;
+  host?: string;
+  user?: string;
+  apiVersion?: string;
+  version?: string;
 
   protected deserialize(res: string): any {
     const obj = JSON.parse(res);
@@ -49,8 +95,6 @@ class GefyraResponse {
 }
 
 export class GefyraStatusResponse extends GefyraResponse {
-  status!: string;
-
   constructor(res: string) {
     super();
     const obj = this.deserialize(res);
@@ -59,8 +103,6 @@ export class GefyraStatusResponse extends GefyraResponse {
 }
 
 export class GefyraUpResponse extends GefyraResponse {
-  status!: string;
-
   constructor(res: string) {
     super();
     const obj = this.deserialize(res);
@@ -69,11 +111,39 @@ export class GefyraUpResponse extends GefyraResponse {
 }
 
 export class GefyraDownResponse extends GefyraResponse {
-  status!: string;
-
   constructor(res: string) {
     super();
     const obj = this.deserialize(res);
     this.status = obj.status;
+  }
+}
+
+export class GefyraRunResponse extends GefyraResponse {
+  response!: GefyraRunInnerResponse; 
+  
+  constructor(res: string) {
+    super();
+    const obj = this.deserialize(res)
+    this.status = obj.status;
+    this.host = obj.host;
+    this.user = obj.user;
+    this.apiVersion = obj.apiVersion;
+    this.version = obj.version;
+    this.response = obj.response;
+  }
+}
+
+export class GefyraBridgeResponse extends GefyraResponse {
+  response!: GefyraBridgeInnerResponse; 
+
+  constructor(res: string) {
+    super();
+    const obj = this.deserialize(res)
+    this.status = obj.status;
+    this.host = obj.host;
+    this.user = obj.user;
+    this.apiVersion = obj.apiVersion;
+    this.version = obj.version;
+    this.response = obj.response;
   }
 }
