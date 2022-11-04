b6808e0ef4e7e567f3821e3c2ce344b9f3dc72b5
Tobias Frölich <tobias@blueshoe.de>
2022-11-04 10:43:53 +0100

    feat: add docker/k8s ts bindings

--- gefyra/src/types.ts
+++ gefyra/src/types.ts
@@ -7,3 +7,43 @@ export type GefyraBridgeInnerResponse = {
   containers: string[],
   bridges: string[]
 }
+
+export type DockerListInnerResponse = {
+  containers: {
+    name: string,
+    id: string,
+    image: string,
+    status: string,
+  },
+}
+
+export type DockerKillRemoveInnerResponse = {
+  success: boolean,
+  container: string,
+}
+
+export type K8sContextInnerResponse = {
+  contexts: string[],
+  active: boolean,
+}
+
+export type K8sNamespaceInnerResponse = {
+  namespaces: string[],
+}
+
+export type K8sWorkloadsInnerResponse = {
+  workloads: {
+    namespace: {
+      name: string,
+    }
+  }
+}
+
+type K8sContainers = {
+  image: string,
+  ports: number[],
+}
+
+export type K8sImagesInnerResponse =  {
+  containers: K8sContainers[],
+}
