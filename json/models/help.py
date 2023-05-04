73a6fcc3c69497c0976753effc65d2e5838d3343
Robert Stein <robert@blueshoe.de>
2023-05-04 16:39:05 +0200

    fix: missing sentryCtx in help request

--- json/models/help.py
+++ json/models/help.py
@@ -2,11 +2,12 @@ from typing import Any
 
 from pydantic import BaseModel
 
-from . import add_action
+from . import SentryContext, add_action
 
 
 @add_action("help")
 class HelpRequest(BaseModel):
+    sentryCtx: SentryContext = SentryContext()
     def __init__(self, **data: Any):
         super().__init__(**data)
 
