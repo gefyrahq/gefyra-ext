e7be209c42a7a7e8778a8224b6a6fe868df919e7
Robert Stein <robert@blueshoe.de>
2023-05-24 15:35:06 +0200

    chore: add debug statement

--- json/main.py
+++ json/main.py
@@ -22,6 +22,7 @@ __VERSION__ = "0.7.16"
 
 def main():
     import docker.transport.npipesocket
+    print("Import workz")
     c = docker.from_env()
     print(c.info())
     import gefyra.configuration
